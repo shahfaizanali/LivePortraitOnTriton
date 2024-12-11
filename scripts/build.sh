04544ebe5892e2ba51907c06911700a1a90e36fa
shahfaizanali <virgo.faizan@hotmail.com>
2024-12-11 14:54:36 +0500

    testing model loading

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,4 +10,4 @@ docker stop live
 
 docker rm live
 
-docker run -d --name live --network host -e PORT=8081 live-portrait
+docker run -d --name live -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints --network host -e PORT=8081 live-portrait
