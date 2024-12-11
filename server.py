2163437abf7949c7fa7e20992d423d81f53c7501
shahfaizanali <virgo.faizan@hotmail.com>
2024-12-11 15:13:51 +0500

    testing model loading

--- server.py
+++ server.py
@@ -60,7 +60,7 @@ image_map = {"default": "deepfake_cleveland.png"}
 
 
 # Assign default values to variables
-default_src_image = "deepfake_cleveland.png"
+default_src_image = "assets/examples/source/s12.jpg"
 default_dri_video = "assets/examples/driving/d14.mp4"
 default_cfg = "configs/trt_infer.yaml"
 default_paste_back = False
@@ -69,7 +69,7 @@ default_paste_back = False
 infer_cfg = OmegaConf.load(default_cfg)
 infer_cfg.infer_params.flag_pasteback = default_paste_back
 pipe = FasterLivePortraitPipeline(cfg=infer_cfg, is_animal=False)
-ret = pipe.prepare_source(default_src_image, realtime=True)
+ret = pipe.prepare_source(default_src_image, realtime=False)
 if not ret:
     print(f"no face in {default_src_image}! exit!")
     exit(1)
