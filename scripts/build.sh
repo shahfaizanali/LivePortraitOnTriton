f9164301f9d56e93a7d6064d766bc43179eda39b
shahfaizanali <virgo.faizan@hotmail.com>
2024-12-16 18:53:12 +0500

    testing one model on triton

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,9 +6,9 @@ git pull
 
 docker build -t live-portrait:triton .
 
-docker stop live
+docker stop live-triton
 
-docker rm live
+docker rm live-triton
 
 cd nginx
 
@@ -22,4 +22,4 @@ docker rm nginx-avatar
 
 docker run --network host -d --name nginx-avatar hypelaunchdev/analytics-nginx
 
-docker run --gpus=all -d --name live -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints --network host -e PORT=8082 live-portrait:triton
+docker run --gpus=all -d --name live-triton -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints --network host -e PORT=8082 live-portrait:triton
