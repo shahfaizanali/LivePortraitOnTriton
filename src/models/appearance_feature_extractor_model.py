b8bcae660874ed9e1694bef671527e8161e14e1e
shahfaizanali <virgo.faizan@hotmail.com>
2024-12-18 14:24:07 +0500

    testing 3 models on triton

--- src/models/appearance_feature_extractor_model.py
+++ src/models/appearance_feature_extractor_model.py
@@ -3,54 +3,45 @@
 # @Email   : wenshaoguo1026@gmail.com
 # @Project : FasterLivePortrait
 # @FileName: motion_extractor_model.py
-import pdb
-import numpy as np
-from .base_model import BaseModel
-import torch
-from torch.cuda import nvtx
-from .predictor import numpy_to_torch_dtype_dict
 
+import numpy as np
+from .new_base_model import BaseModel
+from .new_predictor import get_predictor
 
 class AppearanceFeatureExtractorModel(BaseModel):
     """
-    AppearanceFeatureExtractorModel
+    AppearanceFeatureExtractorModel using Triton for inference
     """
 
     def __init__(self, **kwargs):
         super(AppearanceFeatureExtractorModel, self).__init__(**kwargs)
-        self.predict_type = kwargs.get("predict_type", "trt")
-        print(self.predict_type)
+        self.predictor = get_predictor(model_name="appearance_feature_extractor")
+        if self.predictor is not None:
+            self.input_shapes = self.predictor.input_spec()
+            self.output_shapes = self.predictor.output_spec()
 
     def input_process(self, *data):
-        img = data[0].astype(np.float32)
-        img /= 255.0
-        img = np.transpose(img, (2, 0, 1))
-        return img[None]
+        img = data[0].astype(np.float32) / 255.0
+        img = np.transpose(img, (2, 0, 1))  # HWC to CHW
+        return img[None]  # Add batch dimension
 
     def output_process(self, *data):
+        # Assuming a single output from the model
         return data[0]
 
-    def predict_trt(self, *data):
-        nvtx.range_push("forward")
-        feed_dict = {}
-        for i, inp in enumerate(self.predictor.inputs):
-            if isinstance(data[i], torch.Tensor):
-                feed_dict[inp['name']] = data[i]
-            else:
-                feed_dict[inp['name']] = torch.from_numpy(data[i]).to(device=self.device,
-                                                                      dtype=numpy_to_torch_dtype_dict[inp['dtype']])
-        preds_dict = self.predictor.predict(feed_dict, self.cudaStream)
-        outs = []
-        for i, out in enumerate(self.predictor.outputs):
-            outs.append(preds_dict[out["name"]].cpu().numpy())
-        nvtx.range_pop()
-        return outs
-
     def predict(self, *data):
-        data = self.input_process(*data)
-        if self.predict_type == "trt":
-            preds = self.predict_trt(data)
-        else:
-            preds = self.predictor.predict(data)
-        outputs = self.output_process(*preds)
+        inp = self.input_process(*data)
+        # Create feed_dict for Triton
+        feed_dict = {}
+        inp_meta = self.predictor.inputs[0]
+        feed_dict[inp_meta['name']] = inp.astype(np.float32)
+
+        preds_dict = self.predictor.predict(feed_dict)
+
+        # Gather outputs
+        outs = []
+        for out_meta in self.predictor.outputs:
+            outs.append(preds_dict[out_meta["name"]])
+
+        outputs = self.output_process(*outs)
         return outputs
