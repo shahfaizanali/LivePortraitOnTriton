3552ffb558dc849c8d27e2525a6664b41d78f89c
shahfaizanali <virgo.faizan@hotmail.com>
2024-12-19 00:42:50 +0500

    testing warp

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,7 +4,7 @@
 # @Project : FasterLivePortrait
 # @FileName: __init__.py.py
 
-from .new_warping_spade_model import WarpingSpadeModel
+from .warping_spade_model import WarpingSpadeModel
 from .new_motion_extractor_model import MotionExtractorModel
 from .new_appearance_feature_extractor_model import AppearanceFeatureExtractorModel
 from .new_landmark_model import LandmarkModel
