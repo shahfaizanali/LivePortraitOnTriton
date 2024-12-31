fd393547c9c2a4592cc24452354ae0458a11a519
shahfaizanali <virgo.faizan@hotmail.com>
2024-12-31 15:21:31 +0500

    tested new aysnc predictor 9

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,16 @@ docker stop live-triton
 
 docker rm live-triton
 
+docker stop live-triton-1
+
+docker rm live-triton-1
+
+docker stop live-triton-2
+
+docker rm live-triton-2
+
+
+
 cd nginx
 
 docker build -t hypelaunchdev/analytics-nginx .
@@ -22,4 +32,6 @@ docker rm nginx-avatar
 
 docker run --network host -d --name nginx-avatar hypelaunchdev/analytics-nginx
 
-docker run --gpus=all -d --name live-triton -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints -v /avatar/images:/LivePortraitOnTriton/images --network host -e PORT=8082 live-portrait:triton
+docker run --gpus=all -d --name live-triton -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints -v /avatar/images:/LivePortraitOnTriton/images --network host -e PORT=8081 live-portrait:triton
+docker run --gpus=all -d --name live-triton-1 -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints -v /avatar/images:/LivePortraitOnTriton/images --network host -e PORT=8082 live-portrait:triton
+docker run --gpus=all -d --name live-triton-2 -v /avatar/checkpoints:/LivePortraitOnTriton/checkpoints -v /avatar/images:/LivePortraitOnTriton/images --network host -e PORT=8083 live-portrait:triton
