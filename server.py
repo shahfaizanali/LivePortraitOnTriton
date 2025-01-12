44bfd74416127c22d32315e6d32ace9be38c3df2
shahfaizanali <virgo.faizan@hotmail.com>
2025-01-12 22:42:39 +0500

    reset pose and remove rtmp 5

--- server.py
+++ server.py
@@ -281,7 +281,7 @@ async def offer(request):
         if track.kind == "video":
             local_video = VideoTransformTrack(track, user_id, source_image, merged_cfg)
             relayed = relay.subscribe(local_video, buffered=False)
-            pc.addTrack(local_video)
+            await pc.addTrack(local_video)
             # await create_whip_client(relayed, user_id)
             STREAMS[user_id]["video_track"] = local_video
 
